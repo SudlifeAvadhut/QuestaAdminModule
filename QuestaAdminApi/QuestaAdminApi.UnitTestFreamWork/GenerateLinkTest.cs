5f268a01e40e4491cd0936dd18a36ee46437835f
SudlifeAvadhut <avadhut.parab@sudlife.in>
2024-07-07 17:29:29 +0530

    Unwanted library remove from class

--- QuestaAdminApi/QuestaAdminApi.UnitTestFreamWork/GenerateLinkTest.cs
+++ QuestaAdminApi/QuestaAdminApi.UnitTestFreamWork/GenerateLinkTest.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Text;
-using System.Text.Json.Nodes;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+﻿using System.Net;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using Newtonsoft.Json;
